b780f9071a1ef979d6136dc5b2e8125ad155eced
mohankota <mohanprakash.kota@gmail.com>
2016-10-01 20:13:09 +0530

    Add files via upload

---
+++ prog1.cpp
@@ -0,0 +1,15 @@
+//////////////////////////////////////////////////
+//	FILE:prog1.cpp
+//	ssimple program for using git 
+//
+/////////////////////////////////////////////////
+
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	cout<<"hi this is a simple program to implement git"<<endl;
+	return 0;
+}
