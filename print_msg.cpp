8a36c71903b9d0a1c0302b9f5fa9a94ee7b2c0a7
mohankota <mohanprakash.kota@gmail.com>
2016-10-02 02:41:03 +0530

    change #1 to print_msg.cpp

--- print_msg.cpp
+++ print_msg.cpp
@@ -6,5 +6,9 @@
 void print_msg(void)
 {
 	std::cout<<"hi world this is my first cpp program\n";
+	////////////////
+	//new line added
+	////////////////
+	std::cout<<"github is awesome and its fun to use it\n";
 	return;
 }
